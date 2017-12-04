389a11260d5c443aa003add62ce804c15b864e7f
Guillaume Milan <guillaume.milan@ensimag.grenoble-inp.fr>
2017-12-04 17:28:21 +0100

    BOGS ET WERKZ

--- TPs/squelette/tp2/mini_bus.cpp
+++ TPs/squelette/tp2/mini_bus.cpp
@@ -22,7 +22,7 @@
 #define VIDEO_MEM_ORIGIN 0x2800
 #define SCREEN_ORIGIN 0x10000000
 #define SCREEN_SIZE 0xC
-#define ROM_ADDR 0x20000000
+#define ROM_ADDR 0x00015400
 using namespace std;
 using namespace sc_core;
 
@@ -33,11 +33,26 @@ struct initiator : sc_module {
 	sc_signal < bool , SC_MANY_WRITERS > irq_signal;
 	void thread(void) {
 		ensitlm::data_t val = 0xFFFFFFFF;
+		ensitlm::data_t val_hi = 0xFFFFFFFF;
+		ensitlm::data_t val_lo = 0xFFFFFFFF;
+		ensitlm::data_t val_mask_hi = 0xFFFF0000;
+		ensitlm::data_t val_mask_lo = 0x0000FFFF;
 		ensitlm::addr_t addr = VIDEO_MEM_ORIGIN;
 		for(ensitlm::addr_t i=0x00000000;i<ROM_SIZE; i+=4) {
-		    //socket.read(i+ROM_ADDR,val);
-		    socket.write(i+VIDEO_MEM_ORIGIN,val);
+		    socket.read(i+ROM_ADDR,val);
+		    val_hi = val&val_mask_hi;
+		    val_hi = val_hi | (val_hi >> 16);
+		    val_lo = (val&val_mask_lo)<< 16;
+		    val_lo = val_lo | (val_lo >> 16);
+		    socket.write((i*2)+VIDEO_MEM_ORIGIN,val_hi);
+		    socket.write((i*2+4)+VIDEO_MEM_ORIGIN,val_lo);
 		}
+		for(ensitlm::addr_t i=0x00000000;i<SIZE; i+=4) {
+		    socket.read(i+ORIGIN,val);
+		    cout << "0x" << std::hex << i+ORIGIN << " | ";
+		    cout << "0x" << std::hex << val << endl;
+		}
+
 		cout << "Fin écriture mémoire" << endl;
 		socket.write(SCREEN_ORIGIN+LCDC_ADDR_REG,VIDEO_MEM_ORIGIN);
 		socket.write(SCREEN_ORIGIN+LCDC_START_REG, 0x1);
@@ -82,11 +97,11 @@ int sc_main(int argc, char **argv) {
 	initiator bench("Generator1");
 	Bus router("Bus");
 	Memory mems("Memory",SIZE);
-//	ROM ro_mem("ROM");
+	ROM ro_mem("ROM");
 	/* Bob is mapped at addresses [0, 100[, i.e. ... */
+	router.map(ro_mem.socket, ROM_ADDR, ROM_SIZE);
 	router.map(screen.target_socket, SCREEN_ORIGIN, SCREEN_SIZE);
 	router.map(mems.target, ORIGIN, SIZE);
-	//router.map(ro_mem.socket, ROM_ADDR, ROM_SIZE);
 
 	/* connect components to the bus */
 	bench.socket.bind(router.target);
@@ -96,6 +111,7 @@ int sc_main(int argc, char **argv) {
 	
 	router.initiator.bind(mems.target);
 	router.initiator.bind(screen.target_socket);
+	router.initiator.bind(ro_mem.socket);
 
 	screen.display_int.bind(bench.irq_signal);
 
